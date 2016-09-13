28cf42acaad310d710fc1e02ff8f3fcef1f9b9cc
Brian Mancini <bmancini@gmail.com>
2016-09-13 01:03:35 -0400

    Removed unnecessary start/stop commands

--- src/memory-cache.js
+++ src/memory-cache.js
@@ -3,17 +3,9 @@ import Debug from 'debug';
 const debug = Debug('goodly:memory-cache');
 
 export default async () => {
-  let _cache;
+  let _cache = {};
 
   return {
-    start: async () => {
-      _cache = {};
-    },
-
-    stop: async () => {
-      _cache = undefined;
-    },
-
     readFromCache: async (key) => {
       debug('reading %s', key);
       return _cache[key];
